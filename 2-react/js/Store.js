804e063537ce9e115cf9dbef7f426b44ef711243
Jay Lee <50367569+leejaypower@users.noreply.github.com>
2024-07-24 17:22:44 +0900

    최근 검색어 구현 끝

--- 2-react/js/Store.js
+++ 2-react/js/Store.js
@@ -1,3 +1,4 @@
+import { createNextId } from "./helpers.js";
 import storage from "./storage.js";
 
 const tag = "[Store]";
@@ -12,6 +13,8 @@ class Store {
   }
 
   search(keyword) {
+    this.addHistory(keyword);
+
     return this.storage.productData.filter((product) =>
       product.name.includes(keyword)
     );
@@ -27,6 +30,33 @@ class Store {
 
   _sortHistory(history1, history2) {
     return history2.date > history1.date;
+  }
+
+  addHistory(keyword = "") {
+    keyword = keyword.trim();
+
+    if (!keyword) {
+      return;
+    }
+
+    const hasHistory = this.storage.historyData.some(
+      (history) => history.keyword === keyword
+    );
+
+    if (hasHistory) {
+      this.removeHistory(keyword);
+    }
+
+    const id = createNextId(this.storage.historyData);
+    const date = new Date();
+
+    this.storage.historyData.push({ id, keyword, date });
+    this.storage.historyData = this.storage.historyData.sort(this._sortHistory);
+  }
+  removeHistory(keyword) {
+    this.storage.historyData = this.storage.historyData.filter(
+      (history) => history.keyword !== keyword
+    );
   }
 }
 
--- 2-react/js/main.js
+++ 2-react/js/main.js
@@ -63,8 +63,14 @@ class App extends React.Component {
 
   search(searchKeyword) {
     const searchResult = store.search(searchKeyword);
-    console.log(searchResult);
-    this.setState({ searchKeyword, searchResult, submitted: true });
+    const historyList = store.getHistoryList();
+
+    this.setState({
+      searchKeyword,
+      searchResult,
+      historyList,
+      submitted: true,
+    });
   }
 
   handleReset() {
@@ -74,6 +80,13 @@ class App extends React.Component {
       submitted: false,
       searchResult: [],
     });
+  }
+
+  handleClickRemoveHistory(event, keyword) {
+    event.stopPropagation();
+    store.removeHistory(keyword);
+    const historyList = store.getHistoryList();
+    this.setState({ historyList });
   }
 
   render() {
@@ -142,7 +155,12 @@ class App extends React.Component {
             <li key={id} onClick={() => this.search(keyword)}>
               <span>{keyword}</span>
               <span className="date">{formatRelativeDate(date)}</span>
-              <button className="btn-remove"></button>
+              <button
+                className="btn-remove"
+                onClick={(event) =>
+                  this.handleClickRemoveHistory(event, keyword)
+                }
+              ></button>
             </li>
           );
         })}
