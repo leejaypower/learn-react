a0eef1c68df16850aaefd65bdb68c66f6e95ca76
Jay Lee <50367569+leejaypower@users.noreply.github.com>
2024-07-24 18:47:22 +0900

    함수 컴포넌트

--- 3-component/src/main.js
+++ 3-component/src/main.js
@@ -1,1 +1,6 @@
-console.log("main.js");
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App.js";
+
+// jsx 문법을 사용하려면 'React'도 가져와야한다.
+ReactDOM.render(<App />, document.querySelector("#app"));
