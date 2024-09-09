6989206c85ae60c7be1fe486bfe6b983a48fb725
Jay Lee <50367569+leejaypower@users.noreply.github.com>
2024-09-09 17:07:09 +0900

    useRef 알아보기

--- 4-react-playground/src/components/Register.jsx
+++ 4-react-playground/src/components/Register.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 
 const Register = () => {
   const [input, setInput] = useState({
@@ -7,16 +7,32 @@ const Register = () => {
     job: '',
   });
 
+  const countRef = useRef(0);
+
+  // vue의 ref와도 비슷하군
+  const inputRef = useRef();
+  // state의 변화로 인해 리렌더링이 되도 내부적으로 reset이 되지 않게 설계됨
+
   const onChangeInput = (event) => {
+    countRef.current++;
+    console.log(countRef.current);
     setInput({
       ...input,
       [event.target.name]: event.target.value,
     });
   };
 
+  const onSubmit = () => {
+    if (input.name === '') {
+      // 이름을 입력하는 DOM 요소 focus
+      inputRef.current.focus();
+    }
+  };
+
   return (
     <div>
       <input
+        ref={inputRef}
         name="name"
         value={input.name}
         onChange={onChangeInput}
@@ -39,6 +55,8 @@ const Register = () => {
           <option>궁사</option>
           <option>마법사</option>
         </select>
+
+        <button onClick={onSubmit}>제출</button>
       </div>
     </div>
   );
