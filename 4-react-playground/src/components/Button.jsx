fc6767b63aced8bb25de0d74e4c0b71c2b5bcf3d
Jay Lee <50367569+leejaypower@users.noreply.github.com>
2024-09-09 15:25:30 +0900

    이벤트

--- 4-react-playground/src/components/Button.jsx
+++ 4-react-playground/src/components/Button.jsx
@@ -4,6 +4,11 @@ const Button = ({ text, color, children }) => {
       style={{
         color: color,
       }}
+      onClick={(e) => {
+        console.log(text, e);
+        // e: Synthetic Base Event: 합성 이벤트; 모든 웹 브라우저의 이벤트 객체를 하나로 통일한 형태
+        // 여러 브라우저 규격을 참고해서 하나의 통일된 규격으로 이벤트 객체를 포맷팅 해줘서 크로스 브라우징 이슈를 해결함
+      }}
     >
       {text} - {color.toUpperCase()}
       {children}
