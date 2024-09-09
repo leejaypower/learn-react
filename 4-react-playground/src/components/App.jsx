d04a7f03e4668bdbf2b7502afebb441654de62f5
Jay Lee <50367569+leejaypower@users.noreply.github.com>
2024-09-09 15:43:05 +0900

    리렌더링을 위한 state 핸들링

--- 4-react-playground/src/components/App.jsx
+++ 4-react-playground/src/components/App.jsx
@@ -1,30 +1,36 @@
 import '../App.css';
 import './Header';
 // vite는 확장자 안 써도됨
-import Header from './Header';
-import Main from './Main';
-import Footer from './Footer';
-import Button from './Button';
-// import Button from './Button';
+import { useState } from 'react';
 
 function App() {
-  const buttonProps = {
-    text: '롸롸',
-    color: 'light-blue',
-    a: 1,
-    b: 2,
-    c: 3,
-  };
+  // const state = useState()
+  //  console.log(state); --> state[0]: state의 값, state[1]: 상태 변화함수
+
+  // 따라서 구조 분행하는게 일반적임!
+  const [state, setCount] = useState(0);
+  const [light, setLight] = useState('OFF');
 
   return (
     <>
-      <Button {...buttonProps}></Button>
-      <Button text={'카페'}></Button>
-      <Button text={'블로그'}>
-        {/* children props */}
-        <div>자식 요소</div>
-        <Header></Header>
-      </Button>
+      <div>
+        <h1>{light}</h1>
+        <button
+          onClick={() => {
+            setLight(light === 'ON' ? 'OFF' : 'ON');
+          }}
+        >
+          {light === 'ON' ? 'OFF' : 'ON'}
+        </button>
+      </div>
+      <h1>{state}</h1>
+      <button
+        onClick={() => {
+          setCount(state + 1);
+        }}
+      >
+        +
+      </button>
     </>
   );
 }
