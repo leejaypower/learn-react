77352dc5bbb3b392fa07599161faa0a613449a91
Jay Lee <50367569+leejaypower@users.noreply.github.com>
2024-09-09 16:01:30 +0900

    state/props와 리렌더링

--- 4-react-playground/src/components/App.jsx
+++ 4-react-playground/src/components/App.jsx
@@ -3,26 +3,60 @@ import './Header';
 // vite는 확장자 안 써도됨
 import { useState } from 'react';
 
-function App() {
-  // const state = useState()
-  //  console.log(state); --> state[0]: state의 값, state[1]: 상태 변화함수
+// props나 부모 컴포넌트가 바뀌면 리렌더링
+// => 관련이 없다면 state를 몰아넣지 말고 분리해서 불필요한 리렌더링 방지
+// 파일로 분리하는건 생략하겠음
 
-  // 따라서 구조 분행하는게 일반적임!
-  const [state, setCount] = useState(0);
+const Bulb = ({ light }) => {
+  return (
+    <div>
+      {light === 'ON' ? (
+        <h1
+          style={{
+            backgroundColor: 'orange',
+          }}
+        >
+          ON
+        </h1>
+      ) : (
+        <h1
+          style={{
+            backgroundColor: 'grey',
+          }}
+        >
+          OFF
+        </h1>
+      )}
+    </div>
+  );
+};
+
+const LightButton = () => {
   const [light, setLight] = useState('OFF');
 
   return (
     <>
-      <div>
-        <h1>{light}</h1>
-        <button
-          onClick={() => {
-            setLight(light === 'ON' ? 'OFF' : 'ON');
-          }}
-        >
-          {light === 'ON' ? 'OFF' : 'ON'}
-        </button>
-      </div>
+      <Bulb light={light}></Bulb>
+      <button
+        onClick={() => {
+          setLight(light === 'ON' ? 'OFF' : 'ON');
+        }}
+      >
+        {light === 'ON' ? 'OFF' : 'ON'}
+      </button>
+    </>
+  );
+};
+
+const Counter = () => {
+  // const state = useState()
+  //  console.log(state); --> state[0]: state의 값, state[1]: 상태 변화함수
+
+  // 따라서 구조 분해하는게 일반적임!
+  const [state, setCount] = useState(0);
+
+  return (
+    <div>
       <h1>{state}</h1>
       <button
         onClick={() => {
@@ -31,6 +65,15 @@ function App() {
       >
         +
       </button>
+    </div>
+  );
+};
+
+function App() {
+  return (
+    <>
+      <LightButton></LightButton>
+      <Counter></Counter>
     </>
   );
 }
